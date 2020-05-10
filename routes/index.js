a3b13abc1baaf3d75e69b4d442bc1a0006950c41
Ta-Chung Chen <chenalbert35@gmail.com>
2020-05-09 21:19:29 -0700

    update

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,10 @@ const router = require("express").Router();
 // API Routes
 
 // If no API routes are hit, send the React app
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
 });
 module.exports = router;
